26e8afcc59f387dae32a9ed954fff8d2fda5a87f
Cebrail Shahbazov <cebrail.shahbazov@gmail.com>
2020-12-22 00:05:31 +0400

    launchSetting.json icherisi deyishildi

---
+++ TestingMVC/Controllers/ErrorController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestingMVC.Controllers
+{
+    public class ErrorController : Controller
+    {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [Route("/Error/{StatusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry the resource you requested could not be found";
+                    //ViewBag.Path = statusCodeResult.OriginalPath;
+                    //ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    
+                    _logger.LogWarning($"404 error occured. Path = " +
+                                      $"{statusCodeResult.OriginalPath} and QueryString = " +
+                                      $"{statusCodeResult.OriginalQueryString}");
+                    break;
+            }
+            return View("NotFound");
+        }
+
+        [Route("Error")]
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            var exceptionHandlerPathFeature =
+                HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            //Bu yazilish securty risk dashayir
+            //ViewBag.ExceptionPath = exceptionHandlerPathFeature.Path;
+            //ViewBag.ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
+            //ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
+            _logger.LogError($"The path {exceptionHandlerPathFeature.Path} " +
+                            $"threw an exception {exceptionHandlerPathFeature.Error}");
+            return View("Error");
+        }
+    }
+}
--- TestingMVC/Startup.cs
+++ TestingMVC/Startup.cs
@@ -111,11 +111,12 @@ namespace TestingMVC
             }
             else
             {
-                //app.UseExceptionHandler("Error/");
-                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
